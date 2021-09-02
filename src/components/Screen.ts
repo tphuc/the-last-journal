45679990e5f870102c7c52f4b94b74b416a6dd2a
Tran Bao Phuc <felixtran2000@gmail.com>
2021-09-02 19:49:40 +0700

    footer

--- src/components/Screen.ts
+++ src/components/Screen.ts
@@ -111,7 +111,7 @@ class Screen extends ShadowElement {
                         },
                         '.content-wrapper': {
                             width:"100%",
-                            height:"calc(100vh - 24vh)"
+                            "max-height": '52vh'
                         },
                     },
                     '::-webkit-scrollbar': {
